d80329b1c520562f9aea21348df2e612c25c18ec
Penguin <vanesscapuras@gmail.com>
2024-10-03 12:45:30 +0800

    Depoyment testing

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MyPortfolio/', // Replace with your repo name
-  build: {
-    outDir: 'dist' // Ensure this is set to 'dist'
-  }
 })
